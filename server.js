175619ddb160fa0e9841beeeab61dc24fbc32a04
鸽蛋白 <673115816@qq.com>
2021-09-12 23:39:04 +0800

    public

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
+const path = require('path')
 const express = require('express');
 const app = express();
-// app.use(express.static('public'))
+app.use(express.static(path.resolve(__dirname, 'public')))
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const USERCOUNT = 10
