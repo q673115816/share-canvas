70ecc8e8036d6e60e46b96e1ccfd08435ae385ae
鸽蛋白 <673115816@qq.com>
2021-09-13 14:06:10 +0800

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const path = require('path')
 const express = require('express');
 const app = express();
-app.use(express.static(path.resolve(__dirname, 'public')))
+const INDEX = '/index.html'
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .use((req, res) => res.sendFile(INDEX, {
+        root: path.join(__dirname, 'public')
+    }))
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
